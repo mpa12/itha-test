c609a54f2df21472c6a286470a3bc94aa8abe96f
mpa12 <maksim.28.05@mail.ru>
2024-03-10 16:40:53 +1000

    footer

---
+++ scripts/footer.js
@@ -0,0 +1,22 @@
+(() => {
+    const desktopFooter = document.querySelector('#js-desktop-footer');
+    const mobileFooter = document.querySelector('#js-mobile-footer');
+
+    const footerTemplates = {
+        desktop: desktopFooter,
+        mobile: mobileFooter
+    };
+
+    const footer = document.querySelector('#js-footer');
+
+    const setFooterTemplate = () => {
+        const { innerWidth } = window;
+
+        const version = innerWidth <= 500 ? 'mobile' : 'desktop';
+
+        footer.innerHTML = footerTemplates[version].innerHTML;
+    };
+
+    window.addEventListener('resize', setFooterTemplate);
+    setFooterTemplate()
+})();
